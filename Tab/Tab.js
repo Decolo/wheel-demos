f887fe97036a27a6c1e233c27f3bdfd4b85bb631
decolo <decolo@163.com>
2017-06-24 20:51:13 +0800

    tab-demo change details

--- Tab/Tab.js
+++ Tab/Tab.js
@@ -11,25 +11,24 @@ class Tab {
 
     }
     bindEvent() {
-        let that = this
-        this.$tabHeader.on('click', 'li', function(event) {
-            let $li = $(this)
+        this.$tabHeader.on('click', 'li', (event) => {
+            let $li = $(event.currentTarget)
             let index = $li.index()
-            that.$tabContent.children().removeClass('active')
-            that.$tabHeader.children().removeClass('active')
-            that.$category.removeClass('active')
-            that.$category.eq(index).addClass('active')
-            that.$tabContent.children().eq(index).addClass('active')
+            this.$tabContent.children().removeClass('active')
+            this.$tabHeader.children().removeClass('active')
+            this.$category.removeClass('active')
+            this.$category.eq(index).addClass('active')
+            this.$tabContent.children().eq(index).addClass('active')
             $li.addClass('active')
                 // console.log(this.ctWidth)
-            that.$tabContent.css({
+            this.$tabContent.css({
                 overflow: 'hidden'
             })
-            that.$picBp.eq(index).css({
+            this.$picBp.eq(index).css({
                 position: 'absolute',
-                left: that.ctWidth
+                left: this.ctWidth
             })
-            that.$picBp.eq(index).animate({
+            this.$picBp.eq(index).animate({
                 left: 0
             }, 200)
         })
