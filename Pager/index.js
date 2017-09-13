ceb6f9f56afd2d7cf7a735219bba4fe83dffeda1
Decolo <decolo@163.com>
2017-09-13 12:38:42 +0800

    'new'

---
+++ Pager/index.js
@@ -0,0 +1,14 @@
+// 两个参数count， limit， count总页数， limit每一页的数字按钮个数
+// 当前页数current
+// 根据以上三个初始化
+
+class Pager {
+  constructor(options){
+    this.defaultOptions = {
+      count: 10,
+      currentPage: 1,
+      limit: 10
+    }
+  }
+
+}
