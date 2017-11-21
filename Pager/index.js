2825e29a58ec24ff7410fe4a3d95666b71bd9272
Decolo <decolo@163.com>
2017-11-21 20:48:31 +0800

    'pager'

--- Pager/index.js
+++ Pager/index.js
@@ -1,13 +1,12 @@
-// 两个参数count， limit， count总页数， limit每一页的数字按钮个数
-// 当前页数current
-// 根据以上三个初始化
 
 class Pager {
   constructor(options){
     this.defaultOptions = {
       total: 100,
       initPage: 1,
-      limit: 10
+      limit: 10,
+      callback: null, 
+      params: null
     }
     this.options = Object.assign({}, this.defaultOptions, options)
     this.page = this.options.initPage
@@ -26,23 +25,34 @@ class Pager {
           <button class="last">末页</button>
       </div>
     `
+    // init container
     this.pager = document.createElement('div')
     this.pager.classList.add('pager')
     this.pager.innerHTML = template
+    this.pagesContainer = this.pager.querySelector('.btn-pages')
+    // init button
     this.firstPageBtn = this.pager.querySelector('.first')
-    this.lastPageBtn = this.pager.querySelecotr('.last')
+    this.lastPageBtn = this.pager.querySelector('.last')
     this.preBtn = this.pager.querySelector('.pre')
     this.nextBtn = this.pager.querySelector('.next')
-    this.pagesContainer = this.pager.querySelector('.btn-pages')
-    let { limit } = this.options
-    this.render(this.page, this.page + limit - 1)
+    // init li
+    let { limit, initPage } = this.options
+    this.render(this.page, this.page + limit - 1, initPage)
+    // insert
+    document.body.appendChild(this.pager)
     return this
   }
-  render(start, end) {
+  render(start, end, current) {
     let liStr = ''
     for(let i = start; i <= end; i++) {
-      liStr += `<li>${i}</li>`
+      // comfirm the current
+      if (i === current) {
+        liStr += `<li data-index=${i} class="current-page">${i}</li>`  
+      } else {
+        liStr += `<li data-index=${i}>${i}</li>`
+      }
     }
+    // render lis
     this.pagesContainer.innerHTML = liStr
   }
   bindEvent() {
@@ -50,7 +60,7 @@ class Pager {
     dom.on(this.firstPageBtn, 'click', event => {
       this.gotoPage(1)
     })
-    dom.on(this.firstPageBtn, 'click', event => {
+    dom.on(this.lastPageBtn, 'click', event => {
       this.gotoPage(total)
     })
     dom.on(this.preBtn, 'click', event => {
@@ -59,7 +69,8 @@ class Pager {
     dom.on(this.nextBtn, 'click', event => {
       this.gotoPage(this.page - 1)
     })
-    dom.on(this.pagesContainer, 'li', 'click', event => {
+    console.log(this.pagesContainer)
+    dom.on(this.pagesContainer, 'click', 'li', event => {
       this.gotoPage(Number(event.target.innerText))
     })
   }
@@ -82,8 +93,9 @@ class Pager {
   }
   gotoPage(num) {
     this.page = num
+    // check buttons
     this.checkButton()
-    let { total, limit } = this.options
+    let { total, limit, callback, params } = this.options
     let start = 0, end = 0
     if (num <= Math.ceil(limit / 2)) {
       start = 1
@@ -95,6 +107,31 @@ class Pager {
       start = num - Math.floor(limit / 2)
       end = start + limit - 1
     }
-    this.render(start, end)
+    this.render(start, end, num)
+
+    // callback
+    if (callback) {
+      callback(num, limit)
+    }
+    // url change
+    if (params) {
+      let paramStr = this.parse(params)
+      let url = location.href
+      let index = url.indexOf('?') 
+      if (index === -1) {
+        location.replace(url + paramStr)
+      } else {
+        url = url.substr(0, index) + paramStr
+        location.replace(url)
+      }
+    }
+  }
+  parse(obj) {
+    let arr = []
+    for (let key in obj) {
+      arr.push(`${key}=${encodeURI(obj[key])}`)
+    }
+    return `?${arr.join('&')}`
   }
 }
+
