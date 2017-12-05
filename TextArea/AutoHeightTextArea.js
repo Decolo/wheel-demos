512ec40c4ab21df1b7d2f8d286fa4df0128b5dd7
Decolo <decolo@163.com>
2017-12-05 11:38:03 +0800

    ‘fix

--- TextArea/AutoHeightTextArea.js
+++ TextArea/AutoHeightTextArea.js
@@ -3,17 +3,22 @@ class AutoHeightTextArea {
   constructor(element) {
     this.element = element || null
     this.element.style.resize = 'none'
-    this.element.style
+    this.initHeight = getComputedStyle(this.element)['height']
+    this.curHeight = parseInt(getComputedStyle(this.element)['height'])
     this.bindEvent()
   }
   bindEvent() {
     dom.on(this.element, 'scroll', event => {
       let eventTarget = event.target
       if( eventTarget.scrollHeight > eventTarget.clientHeight ) {
-        eventTarget.style.height = eventTarget.scrollHeight + 'px'
+        this.curHeight = eventTarget.style.height = eventTarget.scrollHeight + 'px'
       }
-      // let height = eventTarget.clientHeight
-      // eventTarget.style.height = height + 5 + 'px'
+    })
+    dom.on(this.element, 'blur' , event => {
+      event.target.style.height = this.initHeight
+    })
+    dom.on(this.element, 'focus' , event => {
+      event.target.style.height = this.curHeight
     })
   }
 }
